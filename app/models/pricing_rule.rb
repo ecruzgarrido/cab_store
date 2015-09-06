5c10a72cfe0f7bfbdbc63dac80ed1741c8d3db32
ecruzgarrido <enrique.cruz.garrido@gmail.com>
2015-09-06 17:55:16 +0100

    Add attributes to pricing rules for checkout

--- app/models/pricing_rule.rb
+++ app/models/pricing_rule.rb
@@ -2,6 +2,10 @@ class PricingRule < ActiveRecord::Base
 
   validates_presence_of :code, :quantity
 
+  validates_presence_of :quantity_free, unless: :accumulate
+
+  validates_presence_of :discount, if: :accumulate
+
   validates_inclusion_of :accumulate, in: [true, false]
 
   validates_uniqueness_of :code
--- spec/models/pricing_rule_spec.rb
+++ spec/models/pricing_rule_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe PricingRule, type: :model do
     it { is_expected.to have_db_column(:code).of_type(:string) }
     it { is_expected.to have_db_column(:accumulate).of_type(:boolean) }
     it { is_expected.to have_db_column(:quantity).of_type(:integer) }
+    it { is_expected.to have_db_column(:quantity_free).of_type(:integer) }
+    it { is_expected.to have_db_column(:discount).of_type(:float) }
     it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
     it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
 
@@ -15,6 +17,19 @@ RSpec.describe PricingRule, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of(:code) }
     it { is_expected.to validate_presence_of(:quantity) }
+
+    context 'if not accumulate' do
+      before { allow(subject).to receive(:accumulate).and_return(false) }
+
+      it { is_expected.to validate_presence_of(:quantity_free) }
+    end
+
+    context 'if not accumulate' do
+      before { allow(subject).to receive(:accumulate).and_return(true) }
+
+      it { is_expected.to validate_presence_of(:discount) }
+    end
+
     it { is_expected.to validate_inclusion_of(:accumulate).in_array([true, false]) }
     it { is_expected.to validate_uniqueness_of(:code) }
   end
